74cd25249ce1d45907b8c0cfdd7495e259997459
Agustin Martinez <agustinmartinez01@gmail.com>
2019-02-06 15:08:08 -0300

    User register and login

--- App.js
+++ App.js
@@ -10,21 +10,20 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Amplify from 'aws-amplify';
+import aws_exports from './src/aws-exports';
+import Home from'./src/login/Home';
+
+Amplify.configure(aws_exports);
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text> Welcome to React Native </Text>
+        <Home/>
+     
       </View>
     );
   }
@@ -32,15 +31,14 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 5,
+   
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 5,
   },
   instructions: {
     textAlign: 'center',
---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const config = {
+	identityPoolId:'us-west-2:e9064ac2-10eb-46c4-83e1-882dfb8cea63',
+	region:'us-west-2',
+	userPoolId:'us-west-2_TATvBVRd3',
+	userPoolWebClientId:'3okjqn2ad69luh493mq6d7onjj'
+}
+export default config
+
