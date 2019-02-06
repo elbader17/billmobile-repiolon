c1655fc5d8140f907bd2518d190a7a30425f070c
Nicolas Santa <nicolas55ar@gmail.com>
2019-02-06 18:03:05 -0300

    Add missing refactor on App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
 import Amplify from 'aws-amplify';
 import aws_exports from './src/aws-exports';
-import Authentication from'./src/components/Authentication';
+import Authentication from './src/components/Authentication';
+import store from '../store';
 
 Amplify.configure(aws_exports);
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Welcome to React Native </Text>
-        <Authentication />
-
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text> Welcome to React Native </Text>
+          <Authentication />
+        </View>
+      </Provider>
     );
   }
 }
