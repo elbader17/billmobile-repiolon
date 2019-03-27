142825d34f712c77894b20139a21c72d6010f165
mdaniotti <tinchodaniotti@gmail.com>
2019-03-27 15:41:07 -0300

    add app-navigator

---
+++ src/components/app-navigator.js
@@ -0,0 +1,62 @@
+import { createStackNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
+import Intro from './Intro';
+import Authentication from './Authentication';
+import TaxConfiguration from './TaxConfiguration/Configure/Component';
+import InitialConfiguration from './TaxConfiguration/InitialConfigure/Component';
+import Loading from './Loading/Component';
+import Home from './Home';
+import NewClient from './Client/Component';
+import Item from './Item/Component';
+import NewItem from './Item/NewItem/Component';
+
+const AppStack = createStackNavigator(
+  {
+    HomeScreen: {
+      screen: Home,
+    },
+    Client: {
+      screen: NewClient,
+    },
+    Items: {
+      screen: Item,
+    },
+    NewItems: {
+      screen: NewItem,
+    },
+  }
+);
+
+const AppTaxConfiguration = createStackNavigator(
+  {
+    InitialConfigure: {
+      screen: InitialConfiguration,
+    },
+    Configure: {
+      screen: TaxConfiguration,
+    }
+  }
+)
+
+const AppLogin = createStackNavigator(
+  {
+    //Intro,
+    Authentication,
+  },
+  {
+    headerMode: 'none',
+  }
+)
+
+const AppNavigator = createAppContainer(createSwitchNavigator(
+  {
+    Loading: Loading,
+    Login: AppLogin,
+    Configuration: AppTaxConfiguration,
+    App: AppStack,
+  },
+  {
+    initialRouteName: 'App',
+  }
+));
+
+export default AppNavigator;
