0682296dcc8b046cb9ef371c254b6305f2952801
mdaniotti <tinchodaniotti@gmail.com>
2019-04-01 13:29:44 -0300

    style client

--- src/components/Client/Component.js
+++ src/components/Client/Component.js
@@ -23,12 +23,28 @@ const conditionIVA = [
   },
 ];
 
+const conditionSale = [
+  {
+    label: 'CONTADO',
+    value: 'contado',
+  },
+  {
+    label: 'DÉBITO',
+    value: 'debito',
+  },
+  {
+    label: 'CRÉDITO',
+    value: 'credito',
+  },
+];
+
 class NewClient extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      condition: '',
+      conditionIva: '',
+      conditionSale: '',
     }
   }
 
@@ -47,43 +63,52 @@ class NewClient extends React.Component {
     return(
       <View style={style.container}>
         <View style={style.container2}>
+          <Text style={style.textRegister}>CONDICIÓN FRENTE AL IVA</Text>
           <View style={ style.textBoxBtnHolder }>
             <Picker
-              selectedValue={this.state.condition}
-              onValueChange={itemValue => this.setState({ condition: itemValue })}>
+              selectedValue={this.state.conditionIva}
+              style = {style.picker}
+              onValueChange={itemValue => this.setState({ conditionIva: itemValue })}>
               {conditionIVA.map((i, index) => (
                 <Picker.Item key={index} label={i.label} value={i.value} />
             ))}
             </Picker>
           </View>
-          <Text>TIPO DE DOCUMENTO</Text>
+          <Text style={style.textRegister}>TIPO DE DOCUMENTO</Text>
           <View style={ style.textBoxBtnHolder }>
             <TextInput
-              placeholder="CUIT O DNI"
+              placeholder="CUIT ó DNI"
               style={ style.textBox }
             />
           </View>
-          <Text style={ style.textRegister }>La información fiscal de los clientes se cargarn automaticamente poniendo su N° de CUIT</Text>
-          <Text>NOMBRE DE FANTASÍA (OPCIONAL)</Text>
+          <Text style={ style.textDescription }>La información fiscal de los clientes se cargarn automaticamente poniendo su N° de CUIT</Text>
+          <View style={style.lineWhite}></View>
+          <Text style={style.textRegister} >NOMBRE DE FANTASÍA (OPCIONAL)</Text>
           <View style={ style.textBoxBtnHolder }>
             <TextInput
               style={ style.textBox }
             />
           </View>
+          <Text style={style.textRegister}>CONDICIÓN DE VENTA</Text>
           <View style={ style.textBoxBtnHolder }>
-            <TextInput
-              placeholder="Condicíon de Venta"
-              style={ style.textBox }
-            />
+            <Picker
+              selectedValue={this.state.conditionSale}
+              style = {style.picker}
+              onValueChange={itemValue => this.setState({ conditionSale: itemValue })}>
+              {conditionSale.map((i, index) => (
+                <Picker.Item key={index} label={i.label} value={i.value} />
+            ))}
+            </Picker>
           </View>
-          <Text style={ style.textRegister }> Esta condicion será la que aparecerá para el cliente per luego pordrás cambiarla en la facturación</Text>
-          <Button
-            title='GUARDAR'
-            onPress={ this.newClient }
-            buttonStyle={ style.submit }
-            titleStyle={ style.submitText }
-          />
+          <Text style={ style.textDescription }> Esta condición será la que aparecerá para el cliente per luego pordrás cambiarla en la facturación</Text>
+          <View style={style.lineWhite}></View>
         </View>
+        <Button
+          title='GUARDAR'
+          onPress={ this.newClient }
+          buttonStyle={ style.submit }
+          titleStyle={ style.submitText }
+        />
       </View>
     )
   }
